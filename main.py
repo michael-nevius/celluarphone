aa3dd4c2422119e5041e557b5b464a61f43b6c97
michael-nevius <michael.a.nevius@gmail.com>
2022-04-01 16:22:32 -0400

    This is my main python fild for the cellphone

---
+++ main.py
@@ -0,0 +1,12 @@
+from cellphone import Cellphone
+myphone = ("Samsung S21+")
+print(myphone.contacts)
+
+myphone.receive_text_message("Hank", "what's going on over at your house I see a bunch of ducks")
+myphone.receive_text_message("Steve", "have you seen my flux capacitor")
+print(myphone.messages)
+
+myphone.create_text_message("Bill", "wanna go play catch?")
+myphone.toggle_vibrate_setting()
+myphone.toggle_vibrate_setting()
+print(myphone.on_vibrate)
